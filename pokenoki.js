e2e0c13956268692d87ea78e8bd359f6c51ed86a
laure-sterne <laure.sterne@gmail.com>
2022-06-05 00:40:39 +0200

    add button and shuffle

--- pokenoki.js
+++ pokenoki.js
@@ -1,25 +1,37 @@
+// set the url of the pokemon api
 const url = 'https://pokeapi-enoki.netlify.app/pokeapi.json';
 
+var pokemons;
+console.log("I am global pokemons before", pokemons)
+
+// call pokemon api
 fetch(url)
-    .then(function(response) { 
-        return response.json()
+    .then(function(response) {
+        return response.json();
     })
     
     .then(function(data) {
-        console.log("le json est là : ", data)
-        let pokemons = data.pokemons;
-        randomPokemon(pokemons)
+        console.log("le json est là : ", data);
+        pokemons = data.pokemons;
+        console.log("la variable pokemons : ", pokemons);
+        
+        displayRandomPokemon();
     })
 
     .catch(function(error) {
-        console.log("there is an error",error);
+        console.log("there is an error", error);
     });
 
-function randomPokemon(pokemons) {
+console.log("I am global pokemons after", pokemons)
+
+
+function displayRandomPokemon() {
+    console.log("pokemons inside displayRandom", pokemons);
 
     // get a random pokemon
-    const pokemon = pokemons[Math.floor(Math.random()*pokemons.length)];
-    console.log("index 0 of pokemons", pokemons[0])
+    var pokemon = pokemons[Math.floor(Math.random()*pokemons.length)];
+
+    // console.log("index 0 of pokemons", pokemons[0])
     const pokemonDisplay = document.getElementById("pokemonImage");
 
     // const backgroundColor 
@@ -28,7 +40,7 @@ function randomPokemon(pokemons) {
     // add name, level and icon
     const pokemonName = pokemon.name;
     const pokemonLevel = pokemon.level;
-    const pokemonIcon = pokemon.abilities[0].icon
+    const pokemonIcon = pokemon.abilities[0].icon;
     document.getElementById("pokemonName").innerText = pokemonName;
     document.getElementById("pokemonLevel").innerText = "lvl" + pokemonLevel + " " + pokemonIcon;
     
@@ -37,38 +49,62 @@ function randomPokemon(pokemons) {
     pokemonImage.src = pokemon.image;
     pokemonImage.alt = pokemon.name;
     pokemonDisplay.appendChild(pokemonImage);
-    pokemonImage.setAttribute("id", "pokemonFigure")
-    document.getElementById("pokemonFigure").setAttribute("alt", pokemon.name)
+    pokemonImage.setAttribute("id", "pokemonFigure");
+    document.getElementById("pokemonFigure").setAttribute("alt", pokemon.name);
     document.getElementById("pokemonFigure").backgroundImage = "url('" + pokemon.image + "')";
     
     // add pokemon abilities
-    const pokemonAbilities = pokemon.abilities
-    console.log("I have all pokemon abilities", pokemonAbilities)
+    const pokemonAbilities = pokemon.abilities;
+    var allAbilities = "";
 
     for (a = 0; a < pokemonAbilities.length; a++){
-        console.log("I am a", a)
-        console.log("get values of a", pokemonAbilities[a])
 
-        const pokemonAbilitiesDisplay = document.getElementById("pokemonAbilities")
-        const abilityInformation = document.createElement("div");
-        abilityInformation.setAttribute("id", pokemonAbilities[a].name + " information")
-        pokemonAbilitiesDisplay.appendChild(abilityInformation)
+        allAbilities += "<div>";
+        allAbilities += "<h2>" + pokemonAbilities[a].icon + " " + pokemonAbilities[a].name + "</h2>";
+        allAbilities += "<h2>" + pokemonAbilities[a].power + "</h2>";
+        allAbilities += "</div>";
+        allAbilities += "<p>" + pokemonAbilities[a].description +"</p>";
+    };
 
-        const abilityName = document.createElement("h2");
-        abilityName.setAttribute("id", pokemonAbilities[a].name)
+    document.getElementById("pokemonAbilities").innerHTML = allAbilities;
+};
 
-        const abilityLevel = document.createElement("h2");
-        abilityLevel.setAttribute("id", pokemonAbilities[a].power)
+// add card in our deck
+function choosePokemon() {
+    console.log("I'm in choosePokemon");
+    console.log("I am pokemons", pokemons);
 
-        const abilityDescription = document.createElement("p");
-        abilityDescription.setAttribute("id", pokemonAbilities[a].name + " description");
+    // to be continued...
+};
+
+// start and stop the shuffle
+var counter = 15;
+var timer;
+
+function changePokemon(){
+    console.log("I start the count");
+    console.log("pokemons", pokemons);
+
+    timer = setInterval(function() {
+        console.log("I am in setInterval");
+
+        counter -= 1;
+        displayRandomPokemon();
+
         
-        abilityInformation.appendChild(abilityName);
-        abilityInformation.appendChild(abilityLevel);
-        pokemonAbilitiesDisplay.appendChild(abilityDescription);
+        document.getElementById("stop").innerText = "Stop (" + counter + " sec)";
 
-        document.getElementById(pokemonAbilities[a].name).innerText = pokemonAbilities[a].icon + " " + pokemonAbilities[a].name;
-        document.getElementById(pokemonAbilities[a].power).innerText = pokemonAbilities[a].power;
-        document.getElementById(pokemonAbilities[a].name + " description").innerText = pokemonAbilities[a].description;
-    }
-}
+        if (counter == 0) {
+            clearInterval(timer);
+        }
+
+    }, 1000);
+
+};
+
+function stopPokemon() {
+    clearInterval(timer);
+    document.getElementById("stop").innerText = "Stop";
+    counter = 15;
+    console.log("I stopped");
+};
